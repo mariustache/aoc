bb83b5610ffdf4c25119f679ac75ca0e86afc116
shardamaka <mariusalexandrutache@gmail.com>
2021-04-11 19:36:46 +0300

    [aoc2015/day1] Added tests and part2 solution.

---
+++ aoc-2015/README.md
@@ -0,0 +1,14 @@
+# Advent of code 2015 challenges
+
+Solved in Rust.
+
+## Commands
+
+Run ```cargo build [--bin bin_name]``` to compile.
+
+Run ```cargo run [--bin bin_name]``` to run executable.
+
+Run ```cargo test [--bin bin_name]``` to execute tests (marked with **#[test]** in each day *main.rs* file).
+
+
+**bin_name** is defined in *Cargo.toml*, under **[[bin]]** entries.
--- aoc-2015/src/day1/main.rs
+++ aoc-2015/src/day1/main.rs
@@ -1,17 +1,23 @@
 use std::env;
 use std::fs;
 
-fn determine_floor(instructions: &String) -> i32 {
+const USIZE_MAX: usize = std::usize::MAX - 1;
+
+fn determine_floor(instructions: &String) -> (i32, usize) {
     let mut floor: i32 = 0;
-    for instruction in instructions.chars() {
+    let mut first_index: usize = USIZE_MAX;
+    for (i, instruction) in instructions.chars().enumerate() {
         match instruction {
             '(' => floor += 1,
             ')' => floor -= 1,
             _ => println!("Invalid character."),
         }
+        if first_index == USIZE_MAX && floor == -1 {
+            first_index = i;
+        }
     }
-
-    return floor;
+    // Position starts from 1 (from challenge description)
+    (floor, first_index + 1)
 }
 
 fn main() {
@@ -20,6 +26,40 @@ fn main() {
     let instructions: String = fs::read_to_string(&args[1])
         .expect("Error reading input file.");
 
+    let (floor_number, first_index) = determine_floor(&instructions);
+    println!("[Part 1] answer is: {}", floor_number);
+    println!("[Part 2] answer is: {}", first_index);
+}
 
-    println!("[Part 1] answer is: {}", determine_floor(&instructions));
+#[test]
+fn test_part1() {
+    let input_expected: Vec<(String, i32)> = vec![
+        (String::from("(())"), 0),
+        (String::from("()()"), 0),
+        (String::from("((("), 3),
+        (String::from("(()(()("), 3),
+        (String::from("))((((("), 3),
+        (String::from("())"), -1),
+        (String::from("))("), -1),
+        (String::from(")))"), -3),
+        (String::from(")())())"), -3)
+    ];
+    
+    for (_input, _expected) in input_expected {
+        let (_floor, _) = determine_floor(&_input);
+        assert_eq!(_floor, _expected, "Floor is {} for input {}", _floor, _input);
+    }
+}
+
+#[test]
+fn test_part2() {
+    let input_expected: Vec<(String, usize)> = vec![
+        (String::from(")"), 1),
+        (String::from("()())"), 5)
+    ];
+    
+    for (_input, _expected) in input_expected {
+        let (_, _position) = determine_floor(&_input);
+        assert_eq!(_position, _expected, "The position is {} for input {}", _position, _input);
+    }
 }
