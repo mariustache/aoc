4120bbc266c3788398edc1cf7143920053101b13
shardamaka <mariusalexandrutache@gmail.com>
2021-04-15 20:34:35 +0300

    [aoc2015/day2] Added part2.

--- aoc-2015/src/day2/main.rs
+++ aoc-2015/src/day2/main.rs
@@ -17,6 +17,17 @@ fn box_area(_box: &Box) -> u32 {
     2 * result + prod_vec.iter().min().unwrap()
 }
 
+fn ribbon_needed(_box: &Box) -> u32 {
+    let mut dimensions = vec![_box.length, _box.width, _box.height];
+    dimensions.sort();
+    let mut ribbon = 0;
+    for dimension in &dimensions[..dimensions.len() - 1] {
+        ribbon += 2 * dimension;
+    }
+    
+    ribbon + (_box.length * _box.height * _box.width)
+}
+
 fn obtain_box(dimensions: Vec<&str>) -> Box {
     assert_eq!(dimensions.len(), 3);
     let string_to_u32 = |dim: &str| -> u32 { dim.parse().unwrap() };
@@ -37,13 +48,36 @@ fn main() {
                 .expect("Error reading input file!");
             
             let mut total_paper: u32 = 0;
+            let mut total_ribbon: u32 = 0;
 
             for dimension in dimensions.lines() {
                 let _box: Box = obtain_box(dimension.split("x").collect());
                 total_paper += box_area(&_box);
+                total_ribbon += ribbon_needed(&_box);
             }
             println!("[Part 1] Square feet of wrapping paper to order: {}", total_paper);
+            println!("[Part 2] Feet of ribbon to order: {}", total_ribbon);
         },
         _ => println!("Too many arguments")
     }
 }
+
+#[test]
+fn test_part2() {
+    let mut _box = Box {
+        length: 2,
+        width: 3,
+        height: 4
+    };
+
+    let total_ribbon = ribbon_needed(&_box);
+    assert_eq!(total_ribbon, 34, "Total ribbon for 2x3x4: {}", total_ribbon);
+
+    _box.length = 1;
+    _box.width = 1;
+    _box.height = 10;
+
+    let total_ribbon = ribbon_needed(&_box);
+    assert_eq!(total_ribbon, 14, "Total ribbon for 1x1x10: {}", total_ribbon);
+}
+
