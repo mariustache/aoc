cb5b87efc9e19423af063db22d5a012951c74c29
shardamaka <mariusalexandrutache@gmail.com>
2021-04-17 18:31:21 +0300

    [aoc2015/day3] Corrected part 2 bug.

--- aoc-2015/README.md
+++ aoc-2015/README.md
@@ -6,7 +6,7 @@ Solved in Rust.
 
 Run ```cargo build [--bin bin_name]``` to compile.
 
-Run ```cargo run [--bin bin_name]``` to run executable.
+Run ```cargo run [--bin bin_name] src/bin_name/input.txt``` to run executable.
 
 Run ```cargo test [--bin bin_name]``` to execute tests (marked with **#[test]** in each day *main.rs* file).
 
--- aoc-2015/src/day3/main.rs
+++ aoc-2015/src/day3/main.rs
@@ -8,21 +8,29 @@ macro_rules! stringify {
     };
 }
 
-fn visited_houses(moves: &String) -> u32 {
-    let mut houses: HashMap<String, u32> = HashMap::new();
+fn update_position(_move: &char) -> (i32, i32) {
     let mut position: (i32, i32) = (0, 0);
-    houses.insert(stringify!(position), 1);
+    match _move {
+        '^' => position.1 += 1,
+        'v' => position.1 -= 1,
+        '>' => position.0 += 1,
+        '<' => position.0 -= 1,
+        _ => println!("Error! Move must be ^ v > <.")
+    }
+
+    position
+}
+
+fn visited_houses(moves: &String, mut houses: HashMap<String, u32>) -> HashMap<String, u32> {
+    let mut position: (i32, i32) = (0, 0);
+    *houses.entry(stringify!(position)).or_insert(1) += 1;
     for _move in moves.chars() {
-        match _move {
-            '^' => position.1 += 1,
-            'v' => position.1 -= 1,
-            '>' => position.0 += 1,
-            '<' => position.0 -= 1,
-            _ => println!("Error! Move must be ^ v > <.")
-        }
+        let increments = update_position(&_move);
+        position.0 += increments.0;
+        position.1 += increments.1;
         *houses.entry(stringify!(position)).or_insert(1) += 1;
     }
-    houses.keys().len() as u32
+    houses
 }
 
 fn split_moves(moves: &String) -> (String, String) {
@@ -46,15 +54,15 @@ fn main() {
         2 => {
             let moves: String = fs::read_to_string(&args[1]).
                 expect("Error reading input file.");
-            let houses = visited_houses(&moves);
-            println!("[Part 1] Number of houses that receive at least one present: {}", houses);
+            let mut houses: HashMap<String, u32> = HashMap::new();
+            houses = visited_houses(&moves, houses);
+            println!("[Part 1] Number of houses that receive at least one present: {}", houses.keys().len());
 
             let (santa, robo_santa) = split_moves(&moves);
-            let santa_houses = visited_houses(&santa);
-            println!("Santa visited: {}", santa_houses);
-            let robo_santa_houses = visited_houses(&robo_santa);
-            println!("Robo-santa visited: {}", robo_santa_houses);
-            println!("[Part 2] Number of houses that receive at least one present: {}", santa_houses + robo_santa_houses - 1);
+            let mut houses: HashMap<String, u32> = HashMap::new();
+            houses = visited_houses(&santa, houses);
+            houses = visited_houses(&robo_santa, houses);
+            println!("[Part 2] Number of houses that receive at least one present: {}", houses.keys().len());
 
         },
         _ => println!("Too many arguments!"),
@@ -67,17 +75,17 @@ fn test_part2() {
     let (santa, robo_santa) = split_moves(&moves);
     assert_eq!(santa, "^^^^^");
     assert_eq!(robo_santa, "vvvvv");
-    let santa_houses = visited_houses(&santa);
-    assert_eq!(santa_houses, 6);
-    let robo_houses = visited_houses(&robo_santa);
-    assert_eq!(robo_houses, 6);
+    let mut houses: HashMap<String, u32> = HashMap::new();
+    houses = visited_houses(&santa, houses);
+    houses = visited_houses(&robo_santa, houses);
+    assert_eq!(houses.keys().len(), 11);
 
     let moves = String::from("^>v<");
     let (santa, robo_santa) = split_moves(&moves);
     assert_eq!(santa, "^v");
     assert_eq!(robo_santa, "><");
-    let santa_houses = visited_houses(&santa);
-    assert_eq!(santa_houses, 2);
-    let robo_houses = visited_houses(&robo_santa);
-    assert_eq!(robo_houses, 2);
+    let mut houses: HashMap<String, u32> = HashMap::new();
+    houses = visited_houses(&santa, houses);
+    houses = visited_houses(&robo_santa, houses);
+    assert_eq!(houses.keys().len(), 3);
 }
